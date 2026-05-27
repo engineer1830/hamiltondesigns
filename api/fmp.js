b18d36c10fed7ecf23909450699516cee8dc1b50
engineer1830 <keepsmiling.hamilton@gmail.com>
2026-05-27 12:39:27 -0700

    push

---
+++ api/fmp.js
@@ -0,0 +1,17 @@
+export default async function handler(req, res) {
+    const url = req.query.url;
+
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+
+        res.setHeader("Access-Control-Allow-Origin", "*");
+        res.status(200).json(data);
+    } catch (err) {
+        res.status(500).json({
+            error: "Proxy error",
+            details: err.toString()
+        });
+    }
+}
+  
