69ab6ff842b488ae1ab2bdaf52e7199abba6afba
engineer1830 <keepsmiling.hamilton@gmail.com>
2026-05-27 14:12:55 -0700

    push

--- api/yahoo_market.js
+++ api/yahoo_market.js
@@ -1,3 +1,9 @@
+export const config = {
+    api: {
+        bodyParser: false
+    }
+};
+  
 export default async function handler(req, res) {
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
